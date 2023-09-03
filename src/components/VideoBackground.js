9a9ebe7efe2eed0231b4fd1585b11b905315488e
Kanha Patidar <kanha.patidar.125@gmail.comm>
2023-09-03 14:21:50 +0530

    "Updated"

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -5,12 +5,15 @@ import { useSelector } from "react-redux";
 const VideoBackground = ({ movieId }) => {
   const trailorVideo = useSelector((store) => store.movies.trailerVideo);
   useMovieTrailer(movieId);
-  // "?&autoplay=1&mute=1"
   return (
     <div className="w-screen">
       <iframe
         className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/" + trailorVideo?.key}
+        src={
+          "https://www.youtube.com/embed/" +
+          trailorVideo?.key +
+          "?&autoplay=1&mute=1"
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
